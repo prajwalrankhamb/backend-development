69edaee1d320813a224b58a68f6f580caa62cd55
prajwalrankhamb <prajwalrankhamb7@gmail.com>
2026-08-06 18:15:03 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
+
 dotenv.config({
     path: './.env'
 })
 
+app.on("error",(error) => {
+    console.log("Error",error);
+    throw error
+})
 
 connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`app is listing on port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGODB Connection Failed !!!", err );
+    
+})
 
 
 
