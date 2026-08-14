25fc2d365e5bf4974a78a3b9fc94453564dff062
prajwalrankhamb <prajwalrankhamb7@gmail.com>
2026-08-14 16:11:11 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//route import
+import userRouter from './routes/user.route.js'
+
+//route declaration
+app.use("/api/v1/users", userRouter)
+
 export { app }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 dotenv.config({
     path: './.env'
-})
+});
 
 app.on("error",(error) => {
     console.log("Error",error);
